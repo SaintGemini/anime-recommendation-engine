5ba8469348b9f38e696d9c2c4a0d72110b27319d
SaintGemini <chacone96@gmail.com>
2022-04-15 10:46:06 -0700

    Fixed redirect errors and passing args

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
-from flask import Flask, redirect, render_template, request, flash, sessions
+from flask import Flask, redirect, render_template, request, flash, session
 import pandas as pd
 import filter as flt
 from flask_sqlalchemy import SQLAlchemy
 import pymysql as sql
 import hashlib
-from os import path
+from datetime import timedelta
 
 
 # create Flask app
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'el rata alada'
+app.permanent_session_lifetime = timedelta(hours=3)
 
 # set up database
 db = sql.connect(host='database-anime-1.c3v8mzuixvpi.us-east-2.rds.amazonaws.com', user='admin', password='animerds', port=3306)
@@ -26,36 +27,29 @@ animes_df = pd.read_csv('data/animes-clean.csv')
 genres = ['Action', 'Adventure', 'Cars', 'Comedy', 'Dementia', 'Demons', 'Drama', 'Ecchi', 'Fantasy', 'Game', 'Harem', 'Hentai', 'Historical', 'Horror', 'Josei', 'Kids', 'Magic', 'Martial Arts', 'Mecha', 'Military', 'Music', 'Mystery', 'Parody', 'Police', 'Psychological', 'Romance', 'Samurai', 'School', 'Sci-Fi', 'Seinen', 'Shoujo Ai', 'Shounen', 'Shounen Ai', 'Slice of Life', 'Space', 'Sports', 'Super Power', 'Supernatural', 'Thriller', 'Vampire', 'Yaoi', 'Yuri']
 decades = ['Pre 1970s', '1970s', '1980s', '1990s', '2000s', '2010s']
 
-top_rated = flt.get_top_rated(10, animes_df)
+top_rated = flt.get_top_rated(40, animes_df)
 
-historical_top_rated = flt.get_top_rated_genre('Historical', 10, animes_df)
-action_top_rated = flt.get_top_rated_genre('Action', 10, animes_df)
-romance_top_rated = flt.get_top_rated_genre('Romance', 10, animes_df)
-old_top_rated = flt.get_top_rated_decade('Pre 1970s', 10, animes_df)
+historical_top_rated = flt.get_top_rated_genre('Historical', 40, animes_df)
+action_top_rated = flt.get_top_rated_genre('Action', 40, animes_df)
+romance_top_rated = flt.get_top_rated_genre('Romance', 40, animes_df)
+old_top_rated = flt.get_top_rated_decade('Pre 1970s', 40, animes_df)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        req = request.form['usr-filter']
-        if req in genres:
-            top_rated_by_filter = flt.get_top_rated_genre(req, 50, animes_df)
-        else:
-            top_rated_by_filter = flt.get_top_rated_decade(req, 50, animes_df)
+        req = request.form['usr-filter']      
+        return redirect('/results/'+req)
 
-        return render_template('results.html',  genres=genres, decades=decades, req=req, top_rated=top_rated_by_filter)
     return render_template('index.html', top_rated=top_rated, action=action_top_rated, romance=romance_top_rated, historical=historical_top_rated, old=old_top_rated, genres=genres, decades=decades)
 
-@app.route('/results', methods=['GET', 'POST'])
-def results():
+@app.route('/results/<req>', methods=['GET', 'POST'])
+def results(req):
     if request.method == 'POST':
         req = request.form['usr-filter']
-        if req in genres:
-            top_rated_by_filter = flt.get_top_rated_genre(req, 50, animes_df)
-        else:
-            top_rated_by_filter = flt.get_top_rated_decade(req, 50, animes_df)
+        return redirect('/results/'+req)
 
-        return render_template('results.html',  genres=genres, decades=decades, req=req, top_rated=top_rated_by_filter)
-
+    top_rated_by_flt = get_top_rated_by_flt(req=req)
+    return render_template('results.html', req=req,  genres=genres, decades=decades, top_rated=top_rated_by_flt)
 
 
 @app.route('/signup', methods=['GET', 'POST'])
@@ -65,65 +59,87 @@ def signup():
             password = request.form['password']
             pass_confirm = request.form['password-confirm']
 
-            print(username, password, pass_confirm)
-            usr_len = len(username)
-            pass_len = len(password)
-            if usr_len < 5 or usr_len > 20:
-                flash('Username must be between 5 and 20 characters.', category='error')
-            elif pass_len < 8 or pass_len > 22:
-                flash('Password must be between 8 and 22 characters.', category='error')
-            elif not username.isalnum() or not password.isalnum():
-                flash('Username/Password most only contain letters and numbers.', category='error')
-            elif password != pass_confirm:
-                flash('Password does not match!', category='error')
+            query = 'SELECT * FROM database_anime.users WHERE username = (%s)'
+            cursor.execute(query,(username))
+            data = cursor.fetchone()
+            if data is None:
+                print(username, password, pass_confirm)
+                usr_len = len(username)
+                pass_len = len(password)
+                if usr_len < 5 or usr_len > 20:
+                    flash('Username must be between 5 and 20 characters.', category='error')
+                elif pass_len < 8 or pass_len > 22:
+                    flash('Password must be between 8 and 22 characters.', category='error')
+                elif not username.isalnum() or not password.isalnum():
+                    flash('Username/Password most only contain letters and numbers.', category='error')
+                elif password != pass_confirm:
+                    flash('Password does not match!', category='error')
+                else:
+                    query = 'INSERT INTO database_anime.users (username, password) VALUES (%s, %s)'
+                    hash_pass = create_hash_password(password=password)
+                    vals = (username, hash_pass)
+                    cursor.execute(query, vals)
+                    db.commit()
+                    flash('Account Created!', category='success')
+                    
             else:
-                query = 'INSERT INTO database_anime.users (username, password) VALUES (%s, %s)'
-                salt = 'ra30a'
-                db_password = salt + password
-                hash_pass = hashlib.md5(db_password.encode()).hexdigest()
-                vals = (username, hash_pass)
-                cursor.execute(query, vals)
-                db.commit()
-                flash('Account Created! Head over to the login page!', category='success')
+                flash('Username already taken. Try another one.', category='error')
 
         return render_template('signup.html')
 
 @app.route('/login', methods=['GET', 'POST'])
 def signin():
+    if 'user' in session:
+        return redirect('/')
     if request.method == 'POST':
-            username = request.form['username']
-            
+            username = request.form['username']          
             password = request.form['password']
-            salt = 'ra30a'
-            db_password = salt + password
-            hash_pass = hashlib.md5(db_password.encode()).hexdigest()
+            hash_pass = create_hash_password(password=password)
 
-            query = 'SELECT username, password from database_anime.users'
-            cursor.execute(query)
-            data =  cursor.fetchall()
-            user_input = (username, hash_pass)
-            if user_input in data:
+            query = 'SELECT * FROM database_anime.users WHERE username = (%s)'
+            cursor.execute(query,(username))
+            user = cursor.fetchone()
+
+            if user == None:
+                flash('Username not recognized', category='error')
+            elif username == user[1] and hash_pass == user[2]:
+                session.permanent = True
+                session['user'] = username
+                session['shows_watched'] = user[3]
                 return redirect('/')
             else:
                 flash('Username or password is incorrect', category='error')
-    return render_template('login.html',  genres=genres, decades=decades)
+    return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    session.pop('user', None)
+    return redirect('/')
+
 
 
 @app.route('/show-info/<show_name>', methods=['GET', 'POST'])
 def show_info(show_name):
     if request.method == 'POST':
         req = request.form['usr-filter']
+        return redirect('/results/'+req)
+
+    show = flt.get_show_by_name(show_name=show_name, df=animes_df)
+    return render_template('show_info.html',  genres=genres, decades=decades, show=show)
+
+
+
+def get_top_rated_by_flt(req):
         if req in genres:
             top_rated_by_filter = flt.get_top_rated_genre(req, 50, animes_df)
         else:
             top_rated_by_filter = flt.get_top_rated_decade(req, 50, animes_df)
+        return top_rated_by_filter
 
-        return render_template('results.html',  genres=genres, decades=decades, req=req, top_rated=top_rated_by_filter)
-
-    show = flt.get_show_by_name(show_name=show_name, df=animes_df)
-    print(show)
-    return render_template('show_info.html',  genres=genres, decades=decades, show=show)
-
+def create_hash_password(password):
+    salt = 'ra30a'
+    db_password = salt + password
+    return hashlib.md5(db_password.encode()).hexdigest()
 
 if __name__ == "__main__":
 
